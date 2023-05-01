6a8799618698878fc60c6f62740afbe8436995b0
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-02 01:28:51 +0300

    Object and links

--- car.js
+++ car.js
@@ -10,3 +10,36 @@ function creatCar(model, color) {
 }
 
 console.log(car.model);
+
+let book = {
+    title: 'название',
+    author: 'Тургенев',
+    price: 100,
+    nPages: 282,
+};
+for(let key in book) {
+    console.log(key+': '+book[key]);
+}
+
+let lib = book; // когда ОБЪЕКТЫ, тут либ будет ссылаться на один и тот-же объект
+// когда речь не об объектах, то создается другая переменная
+lib.title = 'Муму';
+console.log(book.title);
+
+/*let phoneCodes = {
+    '7': 'Россия',
+    '49': 'Германия',
+    '41': 'Швейцария',
+    '1': 'США',
+};*/
+
+let phoneCodes = {
+    '+7': 'Россия',
+    '+49': 'Германия',
+    '+41': 'Швейцария',
+    '+1': 'США',
+};
+
+for(let code in phoneCodes) {
+    console.log(code);  //  !! когда ключи цифры они на выводе будет по порядку, лучше поставить + перед
+};
